adc1534d8608a1e8abe22d92d81c2c5e362db3ad
Fahd-DevOps <fahdkhaled777@gmail.com>
2023-08-22 22:30:05 +0300

    add backup.sh

---
+++ backup.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# Source the library with functions
+source backup_restore_lib.sh
+
+# Validate input parameters
+validate_backup_params "$@"
+
+# Create a directory with the backup date
+backup_date=$(date +'%Y%m%d_%H%M%S')
+backup_dir="${2}/${backup_date}"
+mkdir -p "$backup_dir"
+
+# Loop through directories to backup
+for dir in "${1}"/*; do
+    # Check for modification date to backup only recent files
+    backup_recent_files "$dir" "$backup_dir" "$4"
+done
+
+# Archive and encrypt directories
+backup_directories "$backup_dir" "$3"
+
+# Copy backup to a remote server using scp >> (secure copy) 
+scp -r "$backup_dir" user@remote_server:/path/to/backup/directory
